77885ec1fb6991372432440be915501d4444ba7d
Bruno <brn.milano@hotmail.com>
2024-09-30 13:43:43 -0300

    Alteração na nomenclatura dos arquivos

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 
   /**
+   * @description Page Extensions - Extensões de arquivos de página
+   * - page.tsx, são arquivos de páginas que são renderizados no lado do cliente.
+   * @example index.page.tsx
+   *
+   * - api.ts, são arquivos convertidos em rotas.
+   * @example hello.api.ts
+   */
+  pageExtensions: ["page.tsx", "api.ts", "api.tsx"],
+
+  /**
    * Garante que os source maps sejam gerados para os arquivos JavaScript,
    * o que pode ser útil para depuração de problemas no ambiente de produção,
    * como aqueles relatados pelo Lighthouse.
